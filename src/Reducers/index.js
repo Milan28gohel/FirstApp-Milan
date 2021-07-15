736585c5c07215c9e3c9e8b7b1984b434bc53f66
Milan Gohel <milan@victoryinfotech.com>
2021-07-15 12:03:20 +0530

    space

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,10 +2,9 @@ import { combineReducers } from "redux";
 import postData from "./postReducer";
 import  photoData from "./PhotoReducer";
 
-
-
 const rootReducer = combineReducers({
     postData, photoData
+    
 });
 
 export default rootReducer;
